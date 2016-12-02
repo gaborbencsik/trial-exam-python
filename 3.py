2e313216696e81ce24ec237ebd942de325554b6e
Bencso <gabor.bencsik13@gmail.com>
2016-12-02 19:30:11 +0100

    feat(trial): third exercise

--- 3.py
+++ 3.py
@@ -2,3 +2,20 @@
 # It should take its three sizes as constructor parameters (numbers)
 # It should have a method called `getSurface` that returns the cuboid's surface
 # It should have a method called `getVolume` that returns the cuboid's volume
+
+class Cuboid():
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def getSurface(self):
+        return 2 * (self.x * self.y + self.y * self.z + self.x * self.z)
+
+    def getVolume(self):
+        return self.x * self.y * self.z
+
+cube = Cuboid(2, 3, 4)
+print(cube.getSurface())
+print(cube.getVolume())
